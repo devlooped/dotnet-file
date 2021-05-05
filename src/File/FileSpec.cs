6e0b316a44b15a0dd7878dedda1d87782d06b756
Daniel Cazzulino <daniel@cazzulino.com>
2021-05-05 16:06:51 -0300

    Implement default path heuristics for easier directory structure cloning
    Fixes #37

--- src/File/FileSpec.cs
+++ src/File/FileSpec.cs
@@ -1,9 +1,58 @@
 ﻿using System;
+using System.Linq;
 
 namespace Devlooped
 {
     public class FileSpec
     {
+        public static FileSpec WithDefaultPath(Uri uri)
+            => WithGitHubUri(uri,
+                raw => WithGitHubRawUri(raw,
+                    fallback => new FileSpec(fallback)));
+
+        static FileSpec WithGitHubUri(Uri uri, Func<Uri, FileSpec> next)
+        {
+            var parts = uri.PathAndQuery.Split('/', StringSplitOptions.RemoveEmptyEntries);
+
+            if (uri.Host != "github.com" || parts.Length <= 4)
+                return next(uri);
+
+            // https://github.com/devlooped/dotnet-file/tree/main/src
+            // https://github.com/devlooped/dotnet-file/blob/main/src/Directory.props
+            // https://github.com/devlooped/dotnet-file/raw/main/src/Directory.props
+            if (parts[2] == "tree")
+            {
+                // This is a whole directory URL, so use that as the base dir,
+                // denoted by the ending in a path separator. Note we skip 4 parts 
+                // since those are org/repo/tree/branch, then comes the actual dir.
+                return new FileSpec(string.Join('/', parts.Skip(4)) + "/", uri);
+            }
+            else if (parts[2] == "blob" || parts[2] == "raw")
+            {
+                // This is a specific file URL, so use that as the target path.
+                // Note we skip 4 parts since those are org/repo/[blob/raw]/branch.
+                return new FileSpec(string.Join('/', parts.Skip(4)), uri);
+            }
+            else
+            {
+                // Couldn't be figure it out, just add with no smarts
+                return next(uri);
+            }
+        }
+
+        static FileSpec WithGitHubRawUri(Uri uri, Func<Uri, FileSpec> next)
+        {
+            var parts = uri.PathAndQuery.Split('/', StringSplitOptions.RemoveEmptyEntries);
+
+            // https://raw.githubusercontent.com/kzu/dotnet-file/master/README.md
+            // Raw always points to a specific file URL, so use that as the target path.
+            // Note we skip 4 parts since those are org/repo/[blob/raw]/branch.
+            if (uri.Host == "raw.githubusercontent.com" && parts.Length > 3)
+                return new FileSpec(string.Join('/', parts.Skip(3)), uri);
+
+            return next(uri);
+        }
+
         public FileSpec(Uri uri)
             : this(System.IO.Path.GetFileName(uri.LocalPath), uri)
         {
---
+++ src/Tests/FileSpecTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xunit;
+
+namespace Devlooped
+{
+    public class FileSpecTests
+    {
+        [Theory]
+        [InlineData("https://github.com/devlooped/dotnet-file/tree/main/src", "src/")]
+        [InlineData("https://github.com/devlooped/dotnet-file/blob/main/src/Directory.props", "src/Directory.props")]
+        [InlineData("https://github.com/devlooped/dotnet-file/raw/main/src/Directory.props", "src/Directory.props")]
+        [InlineData("https://raw.githubusercontent.com/kzu/dotnet-file/master/README.md", "README.md")]
+        [InlineData("https://raw.githubusercontent.com/kzu/dotnet-file/master/docs/img/icon.png", "docs/img/icon.png")]
+        public void CalculateDefaultPath(string url, string path)
+        {
+            var spec = FileSpec.WithDefaultPath(new Uri(url));
+
+            Assert.Equal(spec.Path, path);
+        }
+    }
+}
