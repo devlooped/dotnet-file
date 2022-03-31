09e8581276f2f4b9a708f612737ca6097077a372
Atif Aziz <code@raboof.com>
2022-03-31 12:28:22 +0200

    Use docopt.net for command-line parsing

--- src/File/Program.cs
+++ src/File/Program.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Reflection;
 using System.Threading.Tasks;
 using DotNetConfig;
-using Mono.Options;
 
 namespace Devlooped
 {
@@ -13,59 +10,56 @@ namespace Devlooped
     {
         static async Task<int> Main(string[] args)
         {
-            var help = false;
-            string? changelog = null;
-            var options = new OptionSet
+            var result = ProgramArguments.CreateParser()
+                                         .WithVersion(ThisAssembly.Info.Version)
+                                         .Parse(args);
+
+            return await result.Match(RunAsync,
+                                      r => PrintAsync(Console.Out, r.Help.Replace("dotnet-file", "dotnet file")),
+                                      r => PrintAsync(Console.Out, r.Version),
+                                      r => PrintAsync(Console.Error, r.Usage));
+
+            static Task<int> PrintAsync(TextWriter writer, string message, int exitCode = 0)
             {
-                { "?|h|help", "Display this help", h => help = h != null },
-                { "c|changelog:", "Write a changelog", c => changelog = string.IsNullOrEmpty(c) ? "dotnet-file.md" : c },
-            };
+                writer.WriteLine(message);
+                return Task.FromResult(exitCode);
+            }
+        }
 
-            var extraArgs = options.Parse(args);
-            if ((args.Length == 1 && help) || extraArgs.Count == 0)
-                return ShowHelp();
-
+        static async Task<int> RunAsync(ProgramArguments args)
+        {
             var config = Config.Build();
-            var command = extraArgs[0].ToLowerInvariant() switch
+            var command = args switch
             {
-                "add" => new AddCommand(config),
-                "changes" => new ChangesCommand(config),
-                "delete" => new DeleteCommand(config),
-                "init" => new InitCommand(config),
-                "list" => new ListCommand(config),
-                "sync" => new SyncCommand(config),
-                "update" => new UpdateCommand(config),
+                { CmdAdd    : true } => new AddCommand(config),
+                { CmdChanges: true } => new ChangesCommand(config),
+                { CmdDelete : true } => new DeleteCommand(config),
+                { CmdInit   : true } => new InitCommand(config),
+                { CmdList   : true } => new ListCommand(config),
+                { CmdSync   : true } => new SyncCommand(config),
+                { CmdUpdate : true } => new UpdateCommand(config),
                 _ => Command.NullCommand,
             };
 
             if (command == Command.NullCommand)
-                return ShowHelp();
-
-            // Remove first arg which is the command to use.
-            extraArgs.RemoveAt(0);
+            {
+                Console.Error.WriteLine(ProgramArguments.Usage);
+                return 1;
+            }
 
             // Add remainder arguments as if they were just files or urls provided 
             // to the command. Allows skipping the -f|-u switches.
-            var skip = false;
             var files = new List<FileSpec>();
-            for (var i = 0; i < extraArgs.Count; i++)
+            using (var fileOrUrl = args.ArgFileOrUrl.GetEnumerator())
             {
-                if (skip)
-                {
-                    skip = false;
-                    continue;
-                }
-
                 // Try to pair Uri+File to allow intuitive download>path mapping, such as 
                 // https://gitub.com/org/repo/docs/file.md docs/file.md
-                if (Uri.TryCreate(extraArgs[i], UriKind.Absolute, out var uri))
+                if (Uri.TryCreate(fileOrUrl.Current, UriKind.Absolute, out var uri))
                 {
-                    var next = i + 1;
                     // If the next arg is not a URI, use that as the file path for the uri
-                    if (next < extraArgs.Count && !Uri.TryCreate(extraArgs[next], UriKind.Absolute, out _))
+                    if (fileOrUrl.MoveNext() && !Uri.TryCreate(fileOrUrl.Current, UriKind.Absolute, out _))
                     {
-                        files.Add(FileSpec.WithPath(extraArgs[next], uri));
-                        skip = true;
+                        files.Add(FileSpec.WithPath(fileOrUrl.Current, uri));
                     }
                     else
                     {
@@ -82,25 +76,17 @@ namespace Devlooped
 
             var result = await command.ExecuteAsync();
 
-            // If there were changes and a changelog was requested, emit it 
+            // If there were changes and a changelog was requested, emit it
             // to a file.
-            if (changelog != null &&
+            if (args.OptChangelog is var changelog &&
                 command is AddCommand add &&
                 add.Changes.Count > 0 &&
                 GitHub.IsInstalled)
             {
-                GitHub.WriteChanges(changelog!, add.Changes);
+                GitHub.WriteChanges(changelog, add.Changes);
             }
 
             return result;
-        }
-
-        static int ShowHelp()
-        {
-            Console.WriteLine(File.ReadAllText(
-                Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!, "Help.txt")));
-
-            return 0;
         }
     }
 }
