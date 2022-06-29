65f344cb56719b9c2fbe3f00a47ca52a21d9bc0c
Daniel Cazzulino <daniel@cazzulino.com>
2022-06-29 15:12:03 -0300

    Fix formatting

--- src/File/Http/AzureRepoAuthHandler.cs
+++ src/File/Http/AzureRepoAuthHandler.cs
@@ -4,8 +4,8 @@ using System.Net.Http;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.AzureRepos;
 using GitCredentialManager;
+using Microsoft.AzureRepos;
 
 namespace Devlooped
 {
